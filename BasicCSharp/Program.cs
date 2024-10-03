83776c7e1e62a312d66bc553c5fbcc82a06fea30
Hoàng Phúc <113100226+hoangphuc38@users.noreply.github.com>
2024-10-03 12:41:37 +0700

    Handle Converter of Celsuis and Kevin

--- BasicCSharp/Program.cs
+++ BasicCSharp/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using BasicCSharp;
+
+TemperatureConverter converter = new TemperatureConverter();
+
+Console.WriteLine("Please select the convertor direction");
+Console.WriteLine("1. From Celsius to Kevin.");
+Console.WriteLine("2. From Kevin to Celsius.");
+Console.Write("==> ");
+
+string selection = Console.ReadLine();
+
+double F, C = 0;
+
+switch (selection)
+{
+    case "1":
+        Console.Write("Please enter the Celsius temperature: ");
+        F = converter.CelsiusToKevin(Console.ReadLine());
+        Console.WriteLine("Temperature in Kevin: {0:F2}", F);
+        break;
+    case "2":
+        Console.Write("Please enter the Fahrenheit temperature: ");
+        C = converter.KevinToCelsius(Console.ReadLine());
+        Console.WriteLine("Temperature in Celsius: {0:F2}", C);
+        break;
+    default:
+        Console.WriteLine("Please select a convertor.");
+        break;
+}
+
+// Keep the console window open in debug mode.
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
---
+++ BasicCSharp/TemperatureConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCSharp
+{
+    public class TemperatureConverter
+    {
+        /// <summary>
+        /// Convert user's input of Celsius degree to Kevin degree
+        /// </summary>
+        /// <param name="temperatureCelsius">temperatureCelsius</param>
+        /// <returns>After calculating Kevin degree, return Kevin degree in Double format</returns>
+        public double CelsiusToKevin(string temperatureCelsius)
+        {
+            double celsius = Double.Parse(temperatureCelsius);
+
+            double kevin = celsius + 273.15;
+
+            return kevin;
+        }
+
+        /// <summary>
+        /// Convert user's input of Kevin degree to Celsius degree
+        /// </summary>
+        /// <param name="temperatureKevin">temperatureKevin</param>
+        /// <returns>After calculating Celcius degree, return Celsius degree in Double format</returns>
+        public double KevinToCelsius(string temperatureKevin)
+        {
+            double kevin = Double.Parse(temperatureKevin);
+
+            double celsius = kevin - 273.15;
+
+            return celsius;
+        }
+    }
+}
