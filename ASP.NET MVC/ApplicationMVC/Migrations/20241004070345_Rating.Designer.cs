ff595363ce6caa0ff0002a9495304ae033347963
Hoàng Phúc <113100226+hoangphuc38@users.noreply.github.com>
2024-10-04 14:19:24 +0700

    Add function search by genre and title

--- ASP.NET MVC/ApplicationMVC/Migrations/20241004070345_Rating.Designer.cs
+++ ASP.NET MVC/ApplicationMVC/Migrations/20241004070345_Rating.Designer.cs
@@ -4,6 +4,7 @@ using ApplicationMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApplicationMVC.Migrations
 {
     [DbContext(typeof(ApplicationMVCContext))]
-    partial class ApplicationMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20241004070345_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
