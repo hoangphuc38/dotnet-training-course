3e06ed0fa5d451759e7b8139cf5a0b68345d1dc6
Hoàng Phúc <113100226+hoangphuc38@users.noreply.github.com>
2024-10-04 11:21:12 +0700

    Create basic MVC Movie Web

---
+++ ASP.NET MVC/ApplicationMVC/Data/ApplicationMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApplicationMVC.Models;
+
+namespace ApplicationMVC.Data
+{
+    public class ApplicationMVCContext : DbContext
+    {
+        public ApplicationMVCContext (DbContextOptions<ApplicationMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationMVC.Models.Movie> Movie { get; set; } = default!;
+    }
+}
