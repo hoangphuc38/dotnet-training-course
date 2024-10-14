a794343cfe9ab11f03c514bd932dff1ac7d2043b
Hoàng Phúc <113100226+hoangphuc38@users.noreply.github.com>
2024-10-14 17:02:00 +0700

    Add CRUD functions for students, courses and enrollment

---
+++ EFCodeFirst_Migration/Client.cs
@@ -0,0 +1,149 @@
+﻿using EFCodeFirst_Migration.Context;
+using EFCodeFirst_Migration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirst_Migration
+{
+    public class Client
+    {
+        /// <summary>
+        /// Get all students
+        /// </summary>
+        public void GetAllStudent()
+        {
+            using (var db = new DataContext())
+            {
+                var studentList = db.Students.ToList();
+
+                foreach (var student in studentList)
+                {
+                    Console.WriteLine("ID: {0}, FullName: {1}, EnrollmentDate: {2}",
+                        student.ID, student.FullName, student.EnrollmentDate);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add a new student
+        /// </summary>
+        /// <param name="fullName">fullName</param>
+        public void AddStudent(string fullName)
+        {
+            Student student = new Student();
+            student.FullName = fullName;
+
+            using (var db = new DataContext())
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Update student with a new name by ID
+        /// </summary>
+        /// <param name="studentID">studentID</param>
+        /// <param name="fullName">fullName</param>
+        public void UpdateStudent(string studentID, string fullName)
+        {
+            int ID = Convert.ToInt32(studentID);
+
+            using (var db = new DataContext())
+            {
+                var student = db.Students.Where(c => c.ID == ID).FirstOrDefault();
+
+                if (student == null)
+                {
+                    Console.WriteLine("Hoc sinh voi ID nay khong ton tai\n");
+                }
+                else
+                {
+                    student.FullName = fullName;
+                    db.SaveChanges();
+                    Console.WriteLine("Thay doi ten thanh cong\n");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get all courses
+        /// </summary>
+        public void GetAllCourse()
+        {
+            using (var db = new DataContext())
+            {
+                var courseList = db.Courses.ToList();
+
+                foreach (var course in courseList)
+                {
+                    Console.WriteLine("ID: {0}, Title: {1}, Credit: {2}",
+                        course.CourseID, course.Title, course.Credits);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add a new course
+        /// </summary>
+        /// <param name="title">title</param>
+        /// <param name="credit">credit</param>
+        public void AddCourse(string title, string credit)
+        {
+            int creditNumber = Convert.ToInt32(credit);
+
+            Course course = new Course();
+            course.Title = title;
+            course.Credits = creditNumber;
+
+            using (var db = new DataContext())
+            {
+                db.Courses.Add(course);
+                db.SaveChanges();
+                Console.WriteLine("Them khoa hoc moi thanh cong\n");
+            }
+        }
+
+        /// <summary>
+        /// Get all enrollment information
+        /// </summary>
+        public void GetEnrollmentInfo()
+        {
+            using (var db = new DataContext())
+            {
+                var enrollments = db.Enrollments.ToList();
+
+                foreach(var enrollment in enrollments)
+                {
+                    Console.WriteLine("Enrollment ID: {0}, StudentName: {1}, CourseName: {2}, CourseCredit: {3}",
+                        enrollment.EnrollmentID, enrollment.Student.FullName, enrollment.Course.Title, enrollment.Course.Credits);
+                }
+            }
+        }
+
+        /// <summary>
+        /// A student enrolls a course
+        /// </summary>
+        /// <param name="studentID"></param>
+        /// <param name="courseID"></param>
+        public void EnrollACourse(string studentID, string courseID)
+        {
+            int studentId = Convert.ToInt32(studentID); 
+            int courseId = Convert.ToInt32(courseID);   
+
+            Enrollment enrollment = new Enrollment();
+            enrollment.StudentID = studentId;
+            enrollment.CourseID = courseId;
+
+            using (var db = new DataContext())
+            {
+                db.Enrollments.Add(enrollment);
+                db.SaveChanges();
+                Console.WriteLine("Dang ky khoa hoc thanh cong\n");
+            }
+        }
+    }
+}
--- EFCodeFirst_Migration/Program.cs
+++ EFCodeFirst_Migration/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCodeFirst_Migration;
+
+Client client = new Client();
+
+Console.Write("Nhap ten hoc sinh can them: ");
+string fullName = Console.ReadLine();
+client.AddStudent(fullName);
+
+Console.WriteLine("Danh sach tat ca hoc sinh: ");
+client.GetAllStudent();
+Console.WriteLine();
+
+Console.Write("Nhap ID hoc sinh can thay doi ten: ");
+string ID = Console.ReadLine();
+Console.Write("Nhap ten can duoc thay doi: ");
+string studentName = Console.ReadLine();
+client.UpdateStudent(ID, studentName);
+
+Console.Write("Nhap ten khoa hoc can them: ");
+string title = Console.ReadLine();
+Console.Write("Nhap so tin chi: ");
+string credit = Console.ReadLine();
+client.AddCourse(title, credit);
+
+Console.WriteLine("Danh sach tat ca khoa hoc: ");
+client.GetAllCourse();
+Console.WriteLine();
+
+Console.Write("Nhap ID hoc sinh: ");
+string studentID = Console.ReadLine();
+Console.Write("Nhap ID khoa hoc muon dang ky: ");
+string courseID = Console.ReadLine();
+client.EnrollACourse(studentID, courseID);
+
+Console.WriteLine("Thong tin khoa hoc duoc dang ky: ");
+client.GetEnrollmentInfo();
+
